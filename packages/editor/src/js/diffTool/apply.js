fa8df3786a5714686ae8f87e07b27b680695100f
Alever <759636538@qq.com>
2026-06-06 21:15:30 +0800

    [fix] Fixed apply tracked clear mark issue.

--- packages/editor/src/js/diffTool/apply.js
+++ packages/editor/src/js/diffTool/apply.js
@@ -16,7 +16,6 @@ function applyTrackedGroup(editor, groupId, action) {
     }
 
     doc.descendants((node, pos) => {
-        // A. 处理节点 attrs.trackedChange，例如 inlineEquation / listItem / paragraph
         const nodeChange = node.attrs?.trackedChange
 
         if (nodeChange?.groupId === groupId) {
@@ -25,17 +24,12 @@ function applyTrackedGroup(editor, groupId, action) {
 
             if (shouldDelete(nodeChange.type)) {
                 deleteRanges.push({ from, to })
-
-                // 这个节点整体要删，就不要继续深入它的子节点，避免重复删除 paragraph/listItem
                 return false
-            } else {
-                clearNodeAttrPositions.push({ pos, node })
-                // 这里不要 return false，因为你 accept insert 时，
-                // listItem 和内部 paragraph 都可能带 trackedChange，需要一起清掉
             }
+
+            clearNodeAttrPositions.push({ pos })
         }
 
-        // B. 处理文本 mark trackedChange
         if (node.isText && node.marks?.length) {
             const mark = node.marks.find(
                 m => m.type === markType && m.attrs?.groupId === groupId
@@ -48,7 +42,7 @@ function applyTrackedGroup(editor, groupId, action) {
                 if (shouldDelete(mark.attrs.type)) {
                     deleteRanges.push({ from, to })
                 } else {
-                    removeMarkRanges.push({ from, to, mark })
+                    removeMarkRanges.push({ from, to })
                 }
             }
         }
@@ -58,33 +52,56 @@ function applyTrackedGroup(editor, groupId, action) {
 
     let tr = state.tr
 
-    // 很关键：从后往前处理，避免前面的删除导致后面的 pos 偏移
     deleteRanges
         .sort((a, b) => b.from - a.from)
         .forEach(({ from, to }) => {
-            tr.delete(from, to)
+            const mappedFrom = tr.mapping.map(from, -1)
+            const mappedTo = tr.mapping.map(to, 1)
+
+            if (mappedFrom < mappedTo) {
+                tr.delete(mappedFrom, mappedTo)
+            }
         })
 
     removeMarkRanges
         .sort((a, b) => b.from - a.from)
         .forEach(({ from, to }) => {
-            tr.removeMark(from, to, markType)
+            const mappedFrom = tr.mapping.map(from, -1)
+            const mappedTo = tr.mapping.map(to, 1)
+
+            if (mappedFrom < mappedTo) {
+                tr.removeMark(mappedFrom, mappedTo, markType)
+            }
         })
 
     clearNodeAttrPositions
         .sort((a, b) => b.pos - a.pos)
-        .forEach(({ pos, node }) => {
-            // 注意：如果这个节点已经被 delete 覆盖了，可能位置失效。
-            // 所以上面删除和清 attr 最好不要同时作用在同一个节点。
+        .forEach(({ pos }) => {
+            const mapped = tr.mapping.mapResult(pos, -1)
+
+            if (mapped.deleted) {
+                return
+            }
+
+            const currentNode = tr.doc.nodeAt(mapped.pos)
+
+            if (!currentNode?.attrs?.trackedChange) {
+                return
+            }
+
+            if (currentNode.attrs.trackedChange.groupId !== groupId) {
+                return
+            }
+
             const newAttrs = {
-                ...node.attrs,
+                ...currentNode.attrs,
                 trackedChange: null,
             }
 
             try {
-                tr.setNodeMarkup(pos, undefined, newAttrs, node.marks)
+                tr.setNodeMarkup(mapped.pos, undefined, newAttrs, currentNode.marks)
             } catch (e) {
-                // 被前面的 delete 删除掉的节点可以忽略
+                // ignore nodes already invalidated by earlier transaction steps
             }
         })
 
