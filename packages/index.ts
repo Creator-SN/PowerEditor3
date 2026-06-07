ac155dc46b8fedcb1da7aca90087d3919f99ccb9
Alever <759636538@qq.com>
2026-06-07 12:33:50 +0800

    [fix] Fixed types issue.

--- packages/index.ts
+++ packages/index.ts
@@ -37,6 +37,17 @@ declare module 'vue' {
     export interface GlobalComponents {
         DevC: typeof DevC;
         PowerEditor: typeof PowerEditor;
+        'dev-c': typeof DevC;
+        'power-editor': typeof PowerEditor;
+    }
+}
+
+declare module '@vue/runtime-core' {
+    export interface GlobalComponents {
+        DevC: typeof DevC;
+        PowerEditor: typeof PowerEditor;
+        'dev-c': typeof DevC;
+        'power-editor': typeof PowerEditor;
     }
 }
 
